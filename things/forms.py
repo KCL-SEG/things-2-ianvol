c7601a0b35b15fabf41ced03dd65b9bc36d7af8c
Ian Volkov <ian.volkov@kcl.ac.uk>
2023-10-08 16:20:47 +0100

    Thing form v10

--- things/forms.py
+++ things/forms.py
@@ -4,7 +4,7 @@ from .models import Thing
 from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your forms here.
 
-class ThingForm(forms.Form):
+class ThingForm(forms.ModelForm):
     class Meta:
         model = Thing
         fields = ['name', 'description', 'quantity']
