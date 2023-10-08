0717de7a4937d997dd4052a0592481754e8fa939
Ian Volkov <ian.volkov@kcl.ac.uk>
2023-10-08 15:44:27 +0100

    Signup form v1

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,8 @@
 """Forms of the project."""
-
+from django import forms
 # Create your forms here.
+
+class SignUpForm(forms.Form):
+    name = forms.CharField(label = "Name")
+    description = forms.CharField(label = "Description")
+    quantity = forms.IntegerField(label = "Quantity")
--- things/views.py
+++ things/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .forms import SignUpForm
 
 def home(request):
+    form = SignUpForm()
     return render(request, 'home.html', {'form': form})
+    
+
